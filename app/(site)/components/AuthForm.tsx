a27f3c103bb2866d6be92d72f2b894cbb4211353
70928manson <manson972@yahoo.com>
2024-05-23 15:19:52 +0800

    feat: 註冊成功後自動登入

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -51,6 +51,9 @@ const AuthForm = () => {
 
         if (variant === "REGISTER") {
             axios.post('/api/register', data)
+                .then(() => {
+                    signIn("credentials", data)
+                })
                 .catch((err) => {
                     toast.error("Something went wrong!");
                 })
