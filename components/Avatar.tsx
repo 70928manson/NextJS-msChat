cd309428289a8eba5284d3dcac0a6e6aa7078c77
70928manson <manson972@yahoo.com>
2024-05-24 16:13:09 +0800

    feat: 新增Avatar component

---
+++ components/Avatar.tsx
@@ -0,0 +1,15 @@
+import { User } from "@prisma/client"
+
+interface AvatarProps {
+    user: User;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  return (
+    <div>
+      Avatar!
+    </div>
+  )
+}
+
+export default Avatar
